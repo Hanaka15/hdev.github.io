76aa4479b1bf50a7178967890658fd618c79b3d7
Hanaka15 <34461088+Hanaka15@users.noreply.github.com>
2020-10-14 17:20:56 +0200

    optimizing code and images.

--- index.php
+++ index.php
@@ -1,66 +1,58 @@
 <!DOCTYPE html>
 <html>
    <head>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Requiem - TimeSheet 2.0</title>
-
-     <link rel="icon" type="image/png" href="assets/img/logo.png">
-
-     <meta name="title" content="Requiem">
-     <meta name="description" content="We provide a home for anyone and everyone looking for a relaxing place to play games and make new friends along the way.">
-     <meta name="keywords" content="Requiem, Community, Gaming, Sea of Thieves, ARK, Minecraft">
-     <meta name="robots" content="index, follow">
-     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-     <meta name="language" content="English">
-
-     <meta property="og:title" content="Requiem">
-     <meta property="og:image" content="assets/img/logo.png">
-
-
-    <meta property="og:description" content="We provide a home for anyone and everyone looking for a relaxing place to play games and make new friends along the way.">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Requiem - TimeSheet 2.0</title>
+      <link rel="icon" type="image/png" href="assets/img/logo.png">
+      <meta name="title" content="Requiem">
+      <meta name="description" content="We provide a home for anyone and everyone looking for a relaxing place to play games and make new friends along the way.">
+      <meta name="keywords" content="Requiem, Community, Gaming, Sea of Thieves, ARK, Minecraft">
+      <meta name="robots" content="index, follow">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="language" content="English">
+      <meta property="og:title" content="Requiem">
+      <meta property="og:image" content="assets/img/logo.png">
+      <meta property="og:description" content="We provide a home for anyone and everyone looking for a relaxing place to play games and make new friends along the way.">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="stylesheet" href="css/style.css">
    </head>
    <body>
       <div class="container">
          <div class="card">
-           <div class="section">
-             <img class="card-logo" src="assets/img/logo.png">
-           </div>
-
-           <div class="section">
-             <h3>TimeSheet 2.0</h3>
-             <p>To track SOT-Officer hours better we have made a new website that integrates with Discord.</p>
-           </div>
+            <div class="section">
+               <img class="card-logo" src="assets/img/logo.webp">
+            </div>
+            <div class="section">
+               <h3>TimeSheet 2.0</h3>
+               <p>To track SOT-Officer hours better we have made a new website that integrates with Discord.</p>
+            </div>
             <hr>
-              <div class="section">
-                    <ul>
-                        <li>
-                            <i class="material-icons">verified_user</i> <span>Stable and reliable system</span>
-                        </li>
-                        <li>
-                            <i class="material-icons">access_time</i> <span>Calculate time automaticly</span>
-                        </li>
-                        <li>
-                            <i class="material-icons">extension</i> <span>Discord integration for easy access</span>
-                        </li>
-                    </ul>
-                </div>
-                  <hr>
-                <div class="section">
-                  <div class="button">
-                        <div class="button__bg"></div>
-                        <a href="/login.php">
-                            <div class="button__inner">
-                                <i class="material-icons">navigate_next</i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
+            <div class="section">
+               <ul>
+                  <li>
+                     <i class="material-icons">verified_user</i> <span>Stable and reliable system</span>
+                  </li>
+                  <li>
+                     <i class="material-icons">access_time</i> <span>Calculate time automaticly</span>
+                  </li>
+                  <li>
+                     <i class="material-icons">extension</i> <span>Discord integration for easy access</span>
+                  </li>
+               </ul>
+            </div>
+            <hr>
+            <div class="section">
+               <div class="button">
+                  <div class="button__bg"></div>
+                  <a href="/login.php">
+                     <div class="button__inner">
+                        <i class="material-icons">navigate_next</i>
+                     </div>
+                  </a>
+               </div>
             </div>
          </div>
       </div>
+      </div>
    </body>
-   <footer></footer>
 </html>
