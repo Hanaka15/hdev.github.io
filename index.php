4dafd82014681471fb8e9362c78d526914621a19
Hanaka15 <34461088+Hanaka15@users.noreply.github.com>
2020-10-14 17:32:39 +0200

    opt code

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
       <meta property="og:image" content="assets/img/logo.png">
       <meta property="og:description" content="We provide a home for anyone and everyone looking for a relaxing place to play games and make new friends along the way.">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="css/style.css">
    </head>
    <body>
