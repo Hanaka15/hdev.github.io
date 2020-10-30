4d49f263f02deb42e4e67765dda6f10b2d33b584
Hanaka15 <hanakakiller@gmail.com>
2020-10-30 09:39:46 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="core/css/main.css">
-  <link rel="stylesheet" href="core/css/dash.css">
+  <link rel="stylesheet" href="core/css/dashboard.css">
 </head>
 <!-- Header -->
 <?php require 'core/templates/header.php';?>
